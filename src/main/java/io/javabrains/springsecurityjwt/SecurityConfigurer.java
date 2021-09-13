9eba83644cb85397c0d8fcedfa96d7de1aaa8ae2
Antonio <antonio.rodriguez024@gmail.com>
2021-09-13 14:11:49 -0500

    first Commit

---
+++ src/main/java/io/javabrains/springsecurityjwt/SecurityConfigurer.java
@@ -0,0 +1,47 @@
+package io.javabrains.springsecurityjwt;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.beans.factory.annotation.Autowired;
+import io.javabrains.springsecurityjwt.services.MyUserDetailsService;
+
+@EnableWebSecurity
+public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private MyUserDetailsService myUserDetailsService;
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(myUserDetailsService);
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable()
+				.authorizeRequests().antMatchers("/authenticate").permitAll().
+				anyRequest().authenticated();
+				
+	}
+	
+	
+	@Override
+	@Bean
+	protected AuthenticationManager authenticationManager() throws Exception {
+		return super.authenticationManager();
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
+
+
+}
